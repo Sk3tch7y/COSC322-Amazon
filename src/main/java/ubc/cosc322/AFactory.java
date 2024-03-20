bcde0600254693f9658f2fb0b661b206dd943bf1
Adam Badry <57472142+Sk3tch7y@users.noreply.github.com>
2024-03-20 10:00:36 -0700

    added ai

--- src/main/java/ubc/cosc322/AFactory.java
+++ src/main/java/ubc/cosc322/AFactory.java
@@ -4,71 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class AFactory {
-    // TODO: implement the depth scaling factor
-    public static int depth = 5;
 
-    public static int depthCalc(int turn) {
-        /*
-         * since each turn the maximum number of possible moves decreases by a minimum
-         * of one we
-         * can make calculate our branching factor on each turn. since each player has a
-         * maximum
-         * possible 40 moves in all direction based on the queen, 10 on each axis and
-         * diagonal
-         * from the center of the board(this is 1 greater than the actual realty of the
-         * player having to move one queen)
-         * each turn we can subract one square from the possible move set in the future
-         * as the arrow
-         * blocks at least one possible move
-         * thus 4(40)-t is equal to the number of possible branches from a specific
-         * outcome assuming the worst case of the player not blocking any more than one
-         * possible move for each
-         * 
-         * We are going to use this number for our scaling factor to increase the depth
-         * of our search
-         * as the number of possible branches decreases. since this is an upper limit
-         * and the odds of a minimal move being the opponents
-         * first choice, we can adjust this more to expect less of a branching factor
-         * 2 is black and 1 is white, I think 3 is arrows but I cant find it in the
-         * documentation
-         * for example
-         * +---+---+---+---+---+---+---+---+---+---+
-         * | | | | 2 | | | 2 | | | |
-         * +---+---+---+---+---+---+---+---+---+---+
-         * | | | | | | | | | | |
-         * +---+---+---+---+---+---+---+---+---+---+
-         * | | | | | | | | | | |
-         * +---+---+---+---+---+---+---+---+---+---+
-         * | 2 | | | 3 | | | | | | 2 |
-         * +---+---+---+---+---+---+---+---+---+---+
-         * | | | | | | | | | | |
-         * +---+---+---+---+---+---+---+---+---+---+
-         * | | | | | | | | | | |
-         * +---+---+---+---+---+---+---+---+---+---+
-         * | 1 | | | | | | | | | 1 |
-         * +---+---+---+---+---+---+---+---+---+---+
-         * | | | | 1 | | | | | | |
-         * +---+---+---+---+---+---+---+---+---+---+
-         * | | | | | | | | | | |
-         * +---+---+---+---+---+---+---+---+---+---+
-         * | | | | | | | 1 | | | |
-         * +---+---+---+---+---+---+---+---+---+---+
-         * 
-         * As the arrow eliminates major pathways for the top left and left queen
-         * restricting them to only two moves on one axis leaving the next branch to be
-         * 2(5 + 2 + 3 + 2) + (9 + 2+ 3+ 4) + (2+3+5+2+6) which is far less than the 160
-         * possible
-         * branches we predicted on a terrible move. We treat the placement on arrows as
-         * a separate
-         * move as it is a separate action and thus we can expect the branching factor
-         * to be less than 160
-         * since this is a relaxed action generator we must check the possibility of the
-         * actions.
-         */
 
-        return 5;
-    }
-
+    
     public static int[][] deepCopy2DArray(int[][] original) {
         if (original == null) {
             return null;
@@ -116,14 +54,14 @@ public class AFactory {
      * @return The list of possible actions.
      */
     public static ArrayList<Action> ArrowMove(ArrayList<int[][]> movement, int[][] gameState, int[] yourQueen, int[][] yourQueens,
-            int[][] theirQueens) {
+            int[][] theirQueens, boolean turn) {
 
         ArrayList<Action> actions = new ArrayList<Action>();
         for (int[][] move : movement) {
             makeMove(move, gameState);
             ArrayList<int[][]> arrows = Actions(gameState, move[0], theirQueens);
             for (int[][] arrow : arrows) {
-                actions.add(new Action(move, arrow, gameState, yourQueens, theirQueens));
+                actions.add(new Action(move, arrow, gameState, yourQueens, theirQueens, !turn));
             }
             undoMove(move, gameState);
 
@@ -166,11 +104,11 @@ public class AFactory {
 
         return result;
     }
-    public static ArrayList<Action> allMoves(int[][] gameState, int[][] yourQueens, int[][] theirQueens) {
+    public static ArrayList<Action> allMoves(int[][] gameState, int[][] yourQueens, int[][] theirQueens, boolean turn) {
         ArrayList<Action> moves = new ArrayList<Action>();
         ArrayList<int[][]> movement = actionsForAllQueens(gameState, yourQueens, theirQueens);
         for (int[][] move : movement) {
-            moves.addAll(ArrowMove(movement, gameState, move[1], yourQueens, theirQueens));
+            moves.addAll(ArrowMove(movement, gameState, move[1], yourQueens, theirQueens, turn));
         }
         return moves;
     }
@@ -300,12 +238,31 @@ public class AFactory {
         // ArrowMove(result, gameState, yourQueens, theirQueens, depth);
         // System.out.println(Arrays.deepToString(gameState));
         long startTime = System.currentTimeMillis();
+        boolean turn = true;
+        takeAction t = new takeAction();
+        Action a = new Action(gameState, yourQueens, theirQueens, turn, 0);
+        Action best = t.findBestAction(a, 11);
+        System.out.println("Time in seconds: " + (System.currentTimeMillis() - startTime)/1000);
+        System.out.println(best.toString());
+        makeMove(best, gameState);
+        System.out.println(Arrays.deepToString(gameState));
 
-        ArrayList<Action> actions = allMoves(gameState, theirQueens, yourQueens);
-        long endTime = System.currentTimeMillis();
-        long timeElapsed = endTime - startTime;
-        System.out.println(Arrays.deepToString(actions.toArray()).toString());
-        System.out.println("Execution time in seconds: " + timeElapsed);
+    }
 
+    private static void makeMove(Action a, int[][] gameState) {
+        int[][] move = a.getMove();
+        int[][] arrow = a.getArrow();
+        int queen = gameState[move[1][0]][move[1][1]];
+        gameState[move[0][0]][move[0][1]] = queen;
+        gameState[move[1][0]][move[1][1]] = 0;
+        gameState[arrow[0][0]][arrow[0][1]] = -1;
+    }
+    private static void undoMove(Action a, int[][] gameState) {
+        int[][] move = a.getMove();
+        int[][] arrow = a.getArrow();
+        int queen = gameState[move[0][0]][move[0][1]];
+        gameState[move[1][0]][move[1][1]] = queen;
+        gameState[move[0][0]][move[0][1]] = 0;
+        gameState[arrow[0][0]][arrow[0][1]] = 0;
     }
 }
--- src/main/java/ubc/cosc322/COSC322Test.java
+++ src/main/java/ubc/cosc322/COSC322Test.java
@@ -1,7 +1,9 @@
 package ubc.cosc322;
 
 import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 import sfs2x.client.entities.Room;
@@ -9,6 +11,7 @@ import ygraph.ai.smartfox.games.BaseGameGUI;
 import ygraph.ai.smartfox.games.GameClient;
 import ygraph.ai.smartfox.games.GameMessage;
 import ygraph.ai.smartfox.games.GamePlayer;
+import ygraph.ai.smartfox.games.amazons.AmazonsBoard;
 import ygraph.ai.smartfox.games.amazons.AmazonsGameMessage;
 import ygraph.ai.smartfox.games.amazons.HumanPlayer;
 
@@ -21,8 +24,8 @@ import ygraph.ai.smartfox.games.amazons.HumanPlayer;
  */
 public class COSC322Test extends GamePlayer {
 
-	private GameClient gameClient = null;
-	private BaseGameGUI gamegui = null;
+	private static GameClient gameClient = null;
+	private static BaseGameGUI gamegui = null;
 
 	private static String userName = "Adam";
 	private static String passwd = "WoahAPassword";
@@ -30,13 +33,16 @@ public class COSC322Test extends GamePlayer {
 	private static boolean color = true; // true for black, false for white
 	private static int turn = 0;
 	//starting queens
-	public static int[][] blackQueens = {{1, 7}, {4, 1}, {7, 1}, {10, 7}};
-	public static int[][] whiteQueens = {{4, 1}, {1, 4}, {7, 1}, {10, 4}};
+	public static int[][] whiteQueens = { { 3, 0 }, { 0, 3 }, { 0, 6 }, { 3, 9 } };
+	public static int[][] blackQueens = { { 6, 0 }, { 9, 3 }, { 9, 6 }, { 6, 9 } };
+	public static int[][] state = new int[10][10];
+
+	static AmazonsBoard board = new AmazonsBoard();
 
 	public static void main(String[] args) {
 		COSC322Test player = new COSC322Test(userName, passwd);
 		//HumanPlayer player2 = new HumanPlayer();
-
+		
 		if (player.getGameGUI() == null) {
 			player.Go();
 		} else {
@@ -46,9 +52,6 @@ public class COSC322Test extends GamePlayer {
 					player.Go();
 				}
 			});
-		}
-		while(true){
-			System.out.println();
 		}
 	}
 
@@ -80,24 +83,24 @@ public class COSC322Test extends GamePlayer {
 		if (messageType.equals(GameMessage.GAME_STATE_BOARD)) {
 			ArrayList<Integer> gameS = (ArrayList<Integer>) msgDetails.get("game-state");
 			System.out.println("Game Board: " + gameS);
-			gamegui.setGameState(gameS);
+			board.setGameState(gameS);
+			translateArr(gameS);
 		} else if (messageType.equals(GameMessage.GAME_ACTION_START)) {
 
-			this.setColor();
+			this.setColor((String)msgDetails.get(AmazonsGameMessage.PLAYER_BLACK));
 			System.out.println("Game is starting");
-			turn = 0;
 			//move if you are black
 			if (color){
-				
-			}
-			else{
-				turn++;
+				System.out.println("Making a move");
+				handleOpponentMove(msgDetails);
+				//getGameClient().sendMoveMessage( );
 			}
 			
+			
 		} else if (messageType.equals(GameMessage.GAME_ACTION_MOVE)) {
-			System.out.println(msgDetails.get(AmazonsGameMessage.QUEEN_POS_CURR));
+			//System.out.println(msgDetails.get(AmazonsGameMessage.QUEEN_POS_CURR));
 			gamegui.updateGameState(msgDetails);
-			updateQueens(msgDetails);
+			makeMove(msgDetails, color);
 			handleOpponentMove(msgDetails);
 
 		} else if (messageType.equals("user-count-change")) {
@@ -106,9 +109,9 @@ public class COSC322Test extends GamePlayer {
 		return true;
 	}
 
-	private void updateQueens(Map<String, Object> msgDetails) {
-		int[] from = (int[]) msgDetails.get(AmazonsGameMessage.QUEEN_POS_CURR);
-		int[] to = (int[]) msgDetails.get(AmazonsGameMessage.QUEEN_POS_NEXT);
+	private static void updateQueens(int[][] move, boolean color) {
+		int[] from = move[0];
+		int[] to = move[1];
 		//checks and updates the queens
 		if (!color){
 			for (int[] i : blackQueens) {
@@ -129,28 +132,84 @@ public class COSC322Test extends GamePlayer {
 		turn++;
 	}
 
-	static void handleOpponentMove(Map<String, Object> msgDetails) {
+	public static void handleOpponentMove(Map<String, Object> msgDetails) {
 		//Find an action to take and send it
+		//int[][] gameState = translateArr((ArrayList<Integer>) msgDetails.get(AmazonsGameMessage.GAME_STATE));
+		//Action action = new Action(gameState, msgDetails.get(AmazonsGameMessage.ARROW_POS), blackQueens, whiteQueens, blackQueens, color);
+		System.out.println("Making a move");
+		if(msgDetails.get(AmazonsGameMessage.QUEEN_POS_CURR) != null){
+			makeMove(msgDetails, !color);
+		}
+		takeAction t = new takeAction();
+		Action action = new Action(state, blackQueens, whiteQueens, color, 0);
+		Action bestAction = t.findBestAction(action, 8);
+		//send the move
+		int[] next = bestAction.getMove()[0];
+		int[] move = bestAction.getMove()[1];
+		int[] arrow = bestAction.getArrow()[0];
+		ArrayList<Integer> queenFrom = convertToList(move);
+		ArrayList<Integer> queenTo = convertToList(next);
+		ArrayList<Integer> arrowPos = convertToList(arrow);
+		System.out.println(Arrays.toString(move));
+		System.out.println(Arrays.toString(next));
+		System.out.println(Arrays.toString(arrow));
+		makeMove(new int[][]{move, next}, arrow, color);
+		board.updateGameState(queenFrom, queenTo, arrowPos);
+		gameClient.sendMoveMessage(queenFrom, queenTo, arrowPos);
 		
 	}
-	static void translateArr(ArrayList<Integer> arr){
-		int[][] board = new int[10][10];
+	public static ArrayList<Integer> convertToList(int[] a){
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int num : a) {
+			list.add(num+1);
+		}
+		return list;
+	}
+	public static ArrayList<Integer> convertToList(){
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int[] row : state) {
+			for (int num : row) {
+				list.add(num);
+			}
+		}
+		return list;
+	}
+	public static void translateArr(ArrayList<Integer> arr){
 		int count = 0;
 		for (int i = 0; i < 10; i++){
 			for (int j = 0; j < 10; j++){
-				board[i][j] = arr.get(count);
+				state[i][j] = arr.get(count);
 				count++;
 			}
 		}
+	}
 
+	private static void makeMove(Map<String, Object> msgDetails, boolean color) {
+		ArrayList<Integer> QueenCur = (ArrayList<Integer>)msgDetails.get(AmazonsGameMessage.QUEEN_POS_CURR);
+		ArrayList<Integer> QueenNext = (ArrayList<Integer>)msgDetails.get(AmazonsGameMessage.QUEEN_POS_NEXT);
+		ArrayList<Integer> arrowPos = (ArrayList<Integer>)msgDetails.get(AmazonsGameMessage.ARROW_POS);
+		int[] queenFrom = new int[] {QueenCur.get(0)-1, QueenCur.get(1)-1};
+		int[][] move = new int[][] {queenFrom, new int[] {QueenNext.get(0)-1, QueenNext.get(1)-1}};
+		int[] arrow = new int[]{arrowPos.get(0)-1, arrowPos.get(1)-1};
+		updateQueens(move, color);
+		int queen = state[move[1][0]][move[1][1]];
+		state[move[0][0]][move[0][1]] = queen;
+		state[move[1][0]][move[1][1]] = 0;
+		state[arrow[0]][arrow[1]] = -1;
+	}
+	private static void makeMove(int[][]move , int[] arrow, boolean color) {
+		updateQueens(move, color);
+		int queen = state[move[1][0]][move[1][1]];
+		state[move[0][0]][move[0][1]] = queen;
+		state[move[1][0]][move[1][1]] = 0;
+		state[arrow[0]][arrow[1]] = -1;
 	}
 	
-
 	@Override
 	public String userName() {
 		return userName;
 	}
-
+	
 	@Override
 	public GameClient getGameClient() {
 		// TODO Auto-generated method stub
@@ -166,14 +225,15 @@ public class COSC322Test extends GamePlayer {
 	public void connect() {
 		gameClient = new GameClient(userName, passwd, this);
 	}
-	
-	public void setColor(){
-		if (userName.equals("black")){
+	public void setColor(String blackPlayer){
+
+		if (userName.equals(blackPlayer)){
 			color = true;
 		}
 		else{
 			color = false;
 		}
 	}
+	
 
 }// end of class
